da8ed2a98d52f0c7c53576df08725b034a442772
Bertil Ipsen <bertilipsen@Bertils-MacBook-Pro.local>
2018-07-24 15:08:59 +0200

    Commit message

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,6 +34,8 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+THEME = '/Users/bertilipsen/anaconda3/envs/blog-env/lib/python3.5/site-packages/pelican/themes/pelican-themes/elegant'
+
 MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins']
